a96a9ca71ae9b9d693308a980a1f93054d0dce7f
Bhagyamol <74665675+Bhagyamol@users.noreply.github.com>
2021-12-14 09:23:08 +0530

    cycle1

---
+++ Cycle 1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.sjcet.changethecontent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+ TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.tv);
+    }
+
+    public void Change(View view) {
+        tv.setText("Anu");
+    }
+}
